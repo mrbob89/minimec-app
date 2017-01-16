d921b9a63cafd3e5364728356640bcd580ba5a6e
Roman Boboshko <romanboboshko89@gmail.com>
2017-01-16 19:24:36 +0200

    Add menu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import {
     Header,
+    Container,
+    Menu
 } from 'semantic-ui-react';
 
 import './App.css';
@@ -12,6 +14,14 @@ class App extends Component {
                 <div className="App-header">
                     <Header inverted as="h1">Project Mini-Mek</Header>
                 </div>
+                <Container>
+                    <Menu tabular size="massive">
+                        <Menu.Item name="unitInfo" active={true}>Unit Info</Menu.Item>
+                        <Menu.Item name="pilots" active={false}>Pilots</Menu.Item>
+                        <Menu.Item name="mechs" active={false}>Mechs</Menu.Item>
+                        <Menu.Item name="unitOrganization" active={false}>Unit Organization</Menu.Item>
+                    </Menu>
+                </Container>
             </div>
         );
     }
